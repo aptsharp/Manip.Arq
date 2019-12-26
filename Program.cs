6f3bf25a75cc5c31303aa675fb00809241b575f6
Afonso -aptsharp <aff-on@hotmail.com>
2019-12-26 12:47:26 -0300

    File, FileInfo e IOException

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace Manip.Arq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\aff-o\source\repos\Manip.Arq\Msg.txt";
+            /*
+             * sem o @ o caminho do arquivo tem que ficar \\ 
+             * <string sourcePath = "C:\\Users\aff-o\\source\\repos\\Manip.Arq";
+             */
+
+            string targetPath = @"C:\Users\aff-o\source\repos\Manip.Arq\Msg0.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                //estanciar um objeto por onde o arquivo for passar
+
+                fileInfo.CopyTo(targetPath);
+                //para copiar o original para outro arquivo
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                //ira ler todos as linhas do arquivo e guardar em um vetor
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                //todas as linhas do arquivo será mostrado na tela
+
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(e.Message);
+
+            }
+
+
+        }
+    }
+}
+//fonte de dados: https://docs.microsoft.com/pt-br/dotnet/api/system.io.fileinfo.copyto?view=netframework-4.8
